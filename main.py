c6dd64a4dbfc058173cb6b6362ad9bb7b527172e
Long Lin <longlin@cpp.edu>
2019-09-07 01:00:53 -0700

    Finished 1st UI

---
+++ main.py
@@ -0,0 +1,105 @@
+import tkinter as tk
+from tkinter import filedialog, ttk
+from PIL import ImageTk, Image
+import numpy as np
+
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_uploadbutton()
+
+        # Create a Tkinter variable
+        tkvar = tk.StringVar(self)
+
+        # Dictionary with options
+        choices = {'None', 'NN', 'L', 'Bi-linear', 'Bi-cubic'}
+        tkvar.set('None')  # set the default option
+
+
+        dropdownlisFrame = tk.LabelFrame(self)
+        dropdownlisFrame.grid(column=0, row=2, padx=10, pady=10)
+        popupMenu = tk.OptionMenu(dropdownlisFrame, tkvar, *choices)
+        popupMenu.pack(side= tk.TOP)
+        labeltest = tk.Label(dropdownlisFrame, text="Default is None")
+        self.chosenAlgorithm = tkvar
+        labeltest.pack(side=tk.BOTTOM)
+
+        # on change dropdown value
+        def change_dropdown(*args):
+            labeltest.configure(text="The selected item is {}".format(tkvar.get()))
+            self.chosenAlgorithm = format(tkvar.get())
+
+        # link function to change dropdown
+        tkvar.trace('w', change_dropdown)
+        self.create_text_box()
+        self.create_submit_button()
+
+    def create_text_box(self):
+        labelWidth = tk.Label(self, text="Width")
+        labelWidth.grid(column=0, row=9)
+        self.width = tk.Text(self, height=1, width=15)
+        self.width.grid(column=0, row=10)
+
+        labelHeight = tk.Label(self, text="Width")
+        labelHeight.grid(column=0, row=14)
+        self.height = tk.Text(self, height=1, width=15)
+        self.height.grid(column=0, row=15)
+
+        labelBit = tk.Label(self, text="Bit")
+        labelBit.grid(column=0, row=19)
+        self.bit = tk.Text(self, height=1, width=15)
+        self.bit.grid(column=0, row=20)
+
+    def create_submit_button(self):
+        self.submitButton = tk.Button(self, text="Generate", command=self.submitButtonAction)
+        self.submitButton.grid(column=0, row=25, padx=10, pady=10)
+
+    def submitButtonAction(self):
+        print(self.chosenAlgorithm.get())
+        self.convert_to_array()
+
+    def create_uploadbutton(self):
+        self.labelFrame = ttk.LabelFrame(self, text="Open A File")
+        self.labelFrame.grid(column=0, row=0, padx=10, pady=10)
+        self.uploadButton = ttk.Button(self.labelFrame, text="Browse A File", command=self.uploadButtonAction)
+        self.uploadButton.grid(column=0, row=1)
+
+    def uploadButtonAction(self):
+        self.filename = filedialog.askopenfilename(initialdir="/", title="Select A File",
+                                                   filetype=(("jpeg", "*.jpg"), ("All Files", "*.*")))
+        self.label = ttk.Label(self.labelFrame, text="")
+        self.label.grid(column=1, row=2)
+        self.label.configure(text=self.filename)
+        self.show_original_image()
+
+    def show_original_image(self):
+        """
+        self.canvas = tk.Canvas(root, width=300, height=300)
+        self.canvas.pack()
+        self.img = ImageTk.PhotoImage(Image.open(self.filename))
+        self.canvas.create_image(10, 10, anchor=tk.NW, image=self.img)
+        self.canvas.image = self.img
+        """
+        img = Image.open(self.filename)
+        img.show()
+
+    def convert_to_array(self):
+        img = Image.open(self.filename)
+        newimgarray = np.array(img)
+        newimg = Image.fromarray(newimgarray)
+        newimg.show()
+
+    def convert_to_image(self):
+        img = Image.fromarray(self.originalImageArray)
+        img.save("new_file.jpg")
+        img.show("new_file.jpg")
+
+
+root = tk.Tk()
+root.minsize(600, 400)
+
+app = Application(master=root)
+app.mainloop()
